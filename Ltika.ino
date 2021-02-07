b418f404f3d97c5a0c697cb71979194dff132031
harutiro <harutiro2027@outlook.jp>
2021-02-07 22:47:11 +0900

    first commit

---
+++ Ltika.ino
@@ -0,0 +1,33 @@
+// void setup() {
+//   // put your setup code here, to run once:
+//   pinMode(1,OUTPUT);
+//   pinMode(2,OUTPUT);
+//   pinMode(3,OUTPUT);
+//   pinMode(4,OUTPUT);
+
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+
+//   digitalWrite(1,HIGH);
+//   delay(500);
+//   digitalWrite(1,LOW);
+//   delay(500);
+
+//   digitalWrite(2,HIGH);
+//   delay(500);
+//   digitalWrite(2,LOW);
+//   delay(500);
+
+//   digitalWrite(3,HIGH);
+//   delay(500);
+//   digitalWrite(3,LOW);
+//   delay(500);
+
+//   digitalWrite(4,HIGH);
+//   delay(500);
+//   digitalWrite(4,LOW);
+//   delay(500);
+
+// }
